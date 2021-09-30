776c9da860b4e2519fb077afca28a26a0c12c126
Chi Wing Ng <cwng@cern.ch>
2021-09-29 22:20:14 -0400

    Add helper message

--- makeProblem.sh
+++ makeProblem.sh
@@ -5,6 +5,10 @@
 problemId=$1
 problemTitle=$2
 
+if [[ "$problemId" = "-h" ]]; then
+  echo "Usage of this script: "$0" [problemId] [problemTitle]."
+fi
+
 mkdir -p ${problemId}-${problemTitle}/
 touch ${problemId}-${problemTitle}/${problemId}-${problemTitle}_cppSolution.cxx
 touch ${problemId}-${problemTitle}/${problemId}-${problemTitle}_pythonSolution.py
