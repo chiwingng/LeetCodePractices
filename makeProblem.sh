e1cc2298bb09636b87f5c24088459788db8a981b
Chi Wing Ng <cwng@cern.ch>
2021-12-08 23:00:31 -0500

    Generate a template README while generating a problem directory

--- makeProblem.sh
+++ makeProblem.sh
@@ -13,3 +13,19 @@ fi
 mkdir -p ${problemId}-${problemTitle}/
 touch ${problemId}-${problemTitle}/${problemId}-${problemTitle}_cppSolution.cxx
 touch ${problemId}-${problemTitle}/${problemId}-${problemTitle}_pythonSolution.py
+
+readmefile=${problemId}-${problemTitle}/README.md
+
+echo "# ${problemId}. "$(echo ${problemTitle} | sed 's/_/ /g') > $readmefile
+echo  >> $readmefile
+echo "LeetCode Problem Link: <https://leetcode.com/problems/"$(echo ${problemTitle} | sed 's/_/-/g' | tr '[:upper:]' '[:lower:]')"/>">> $readmefile
+echo  >> $readmefile
+echo "## Description" >> $readmefile
+echo  >> $readmefile
+echo  >> $readmefile
+echo  >> $readmefile
+echo "## Constraints" >> $readmefile
+echo  >> $readmefile
+echo  >> $readmefile
+echo  >> $readmefile
+echo "## Examples" >> $readmefile
